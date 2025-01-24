0e272faa306596cdd7fd811571231be24d27d3e3
Jianan <Jianan@web.de>
2025-01-25 00:57:20 +0100

    make the mobs chase the player #3

--- entities/mobs/mob.gd
+++ entities/mobs/mob.gd
@@ -1,6 +1,64 @@
 extends EntityStateMachine
+class_name Mob
+
+@export var movement_speed: float = 200.0
+@export var detection_range: float = 1000.0
+@export var min_distance: float = 100.0  # Minimum distance to keep from player
+
+var target: Node2D = null
+var direction: Vector2 = Vector2.ZERO
 
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+func _ready() -> void:
+	super._ready()
+	# Try to find player
+	find_player()
+
+func _physics_process(delta: float) -> void:
+	if is_dead:
+		return
+		
+	if target:
+		chase_target(delta)
+	else:
+		find_player()
+
+func find_player() -> void:
+	target = get_tree().get_first_node_in_group("player")
+
+func chase_target(delta: float) -> void:
+	if !is_instance_valid(target):
+		target = null
+		return
+		
+	var distance = global_position.distance_to(target.global_position)
+	
+	# Only chase if within detection range and further than minimum distance
+	if distance < detection_range and distance > min_distance:
+		# Calculate direction to player
+		direction = (target.global_position - global_position).normalized()
+		
+		# Move towards player
+		velocity = direction * movement_speed
+		
+		# Update sprite direction
+		if has_node("AnimatedSprite2D"):
+			var sprite = get_node("AnimatedSprite2D")
+			if direction.x != 0:
+				sprite.flip_h = direction.x < 0
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
+
+func take_hit(damage: float, damage_type: String = "physical") -> float:
+	var final_damage = super.take_hit(damage, damage_type)
+	
+	if current_hp <= 0:
+		die()
+	
+	return final_damage
 
 func die() -> void:
 	if current_state != PlayerState.DYING:
