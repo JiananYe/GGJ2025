41182f774fe6c09ff26634e796177c6828f31832
CappedMonke <schuergersilasdev@gmail.com>
2025-01-26 11:43:37 +0100

    working bubble shader

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -13,9 +13,12 @@ signal start_game_signal
 
 
 var is_main_menu_displaying := true
+var is_bubble_walking_in := false
 var shader_bubble_init_center := Vector2(0.5, 0.5)
+var shader_bubble_walk_in_speed := 0.3
+var shader_bubble_walk_in_position := Vector2(0.5, 1.5)
 var shader_bubble_center := Vector2(0.5, 0.5)
-var shader_bubble_amplitude := Vector2(0.03, 0.07)
+var shader_bubble_amplitude := Vector2(0.05, 0.06)
 
 
 func _ready() -> void:
@@ -23,25 +26,34 @@ func _ready() -> void:
 	for node in get_tree().get_nodes_in_group("game_overlay"):
 		node.hide()
 	hide_death_menu()
+	shader_bubble_center = shader_bubble_walk_in_position
+	move_shader_bubble_center(shader_bubble_walk_in_position)
+	is_bubble_walking_in = true
 
 
 func _process(delta: float) -> void:
+	if is_bubble_walking_in:
+		var distance = shader_bubble_center.y - shader_bubble_init_center.y
+		var easing_factor = (distance * 7) if distance < 0.1 else 1.0  # Ease only at the very end
+		shader_bubble_center.y -= shader_bubble_walk_in_speed * delta * easing_factor
+		move_shader_bubble_center(shader_bubble_center)
+		if shader_bubble_center.y <= shader_bubble_init_center.y:
+			is_bubble_walking_in = false
+	#
 	if is_main_menu_displaying:
-		var shader = bubble_shader.material
 		var time = Time.get_ticks_msec() / 1000.0
-		shader_bubble_center.x = shader_bubble_init_center.x + sin(time) * shader_bubble_amplitude.x
-		shader_bubble_center.y = shader_bubble_init_center.y + cos(time * 0.5) * shader_bubble_amplitude.y
-		(shader as ShaderMaterial).set_shader_parameter("effect_center", shader_bubble_center)
+		float_shader_bubble(time)
+
 
 
 func trigger_death_menu():
-	you_died.modulate.a = 0
-	black_background.modulate.a = 0
-	you_died.show()
-	black_background.show()
-	await fade_in(you_died, 2.5)
-	await fade_in(black_background, 2)
-	await get_tree().create_timer(3).timeout
+	#you_died.modulate.a = 0
+	#black_background.modulate.a = 0
+	#you_died.show()
+	#black_background.show()
+	#await fade_in(you_died, 2.5)
+	#await fade_in(black_background, 2)
+	#await get_tree().create_timer(3).timeout
 	death_menu_fade_finished.emit()
 
 
@@ -61,3 +73,14 @@ func hide_death_menu():
 func start_game():
 	for node in get_tree().get_nodes_in_group("game_overlay"):
 		node.show()
+
+
+func move_shader_bubble_center(pos: Vector2) -> void:
+	var shader = bubble_shader.material
+	(shader as ShaderMaterial).set_shader_parameter("effect_center", pos)
+
+
+func float_shader_bubble(time: float) -> void:
+	var center_x = shader_bubble_center.x + sin(time) * shader_bubble_amplitude.x
+	var center_y = shader_bubble_center.y + cos(time * 0.5) * shader_bubble_amplitude.y
+	move_shader_bubble_center(Vector2(center_x, center_y))
