108dc4b9d600fac5e233af517a00bc4a6a2fe0e9
CappedMonke <schuergersilasdev@gmail.com>
2025-01-26 10:04:17 +0100

    working bubble shader, now letting the bubble fly in

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -3,20 +3,35 @@ extends Control
 
 
 signal death_menu_fade_finished
+signal start_game_signal
 
 
 @onready var death_menu: Control = $DeathMenu
 @onready var black_background: Panel = $DeathMenu/BlackBackground
 @onready var you_died: MarginContainer = $DeathMenu/YouDied
+@onready var bubble_shader: ColorRect = $MainMenu/CanvasLayer/TextureRect
+
+
+var is_main_menu_displaying := true
+var shader_bubble_init_center := Vector2(0.5, 0.5)
+var shader_bubble_center := Vector2(0.5, 0.5)
+var shader_bubble_amplitude := Vector2(0.03, 0.07)
 
 
 func _ready() -> void:
 	DirtyDirtyUiManager.main_menu = self
+	for node in get_tree().get_nodes_in_group("game_overlay"):
+		node.hide()
 	hide_death_menu()
 
 
 func _process(delta: float) -> void:
-	pass
+	if is_main_menu_displaying:
+		var shader = bubble_shader.material
+		var time = Time.get_ticks_msec() / 1000.0
+		shader_bubble_center.x = shader_bubble_init_center.x + sin(time) * shader_bubble_amplitude.x
+		shader_bubble_center.y = shader_bubble_init_center.y + cos(time * 0.5) * shader_bubble_amplitude.y
+		(shader as ShaderMaterial).set_shader_parameter("effect_center", shader_bubble_center)
 
 
 func trigger_death_menu():
@@ -41,3 +56,8 @@ func fade_in(node: CanvasItem, duration: float) -> void:
 func hide_death_menu():
 	black_background.modulate.a = 0
 	you_died.modulate.a = 0
+
+
+func start_game():
+	for node in get_tree().get_nodes_in_group("game_overlay"):
+		node.show()
